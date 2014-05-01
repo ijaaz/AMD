a2e9e45bb0a6cc6efcff5a29f63c3cde92c5458b
Muhammad Tello <mtello@flextech-hs.org>
2014-04-30 20:34:31 -0400

    Finished util/array.js
    Added som utils for a global forEach.

--- js/utils/array.js
+++ js/utils/array.js
@@ -1,4 +1,6 @@
+/*global define*/
 define(function () {
+  
   var nativeForEach = function (list, callback, thisArg) {
     [].forEach.call(list, callback, thisArg);
   },
@@ -7,4 +9,35 @@ define(function () {
 
     if ( list == null ) {
       throw new TypeError( " this is null or not defined" );
-  }
+    }
+
+    O = Object(list);
+    len = O.length >>> O;
+
+    if ( {}.toString.call(callback) != "[object Function]" ) {
+      throw new TypeError( callback + " is not a function" );
+    }
+
+    if ( thisArg ) {
+      T = thisArg;
+    }
+
+    while( k < len ) {
+      if ( k in O ) {
+        kValue = O[k];
+        callback.call(T, kValue, k, O);
+      }
+      k++;
+    }
+  };
+
+  return {
+    forEach: (function () {
+      if ({}.toString.call([].forEach) === "[object Function]") {
+        return nativeForEach;
+      } else {
+        return customForEach;
+      }
+    }())
+  };
+});

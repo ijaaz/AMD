86f53f1177c794e0d033883b83cb6442f3d205fd
Muhammad Tello <mtello@flextech-hs.org>
2014-04-30 20:48:27 -0400

    Just used my helper functions
    Just used my new click function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 require(['dom/events'], function (events) {
   var elem = document.getElementById('target');
-  events.addEvent(elem, 'click', function () {
+  events.click(elem, function () {
     alert('clicked');
   });
 });
